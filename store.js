5c51c9417ada4332e7c8beb6c2d69b8c6b2b8570
dhenu <dhenu@myseneca.ca>
2023-08-06 19:03:31 -0400

    initial commit

--- store.js
+++ store.js
@@ -1,4 +1,5 @@
 import { atom } from 'jotai';
-
+export const isAuthenticatedAtom = atom(false); 
 export const favouritesAtom = atom([]);
 export const searchHistoryAtom = atom([]);
+export const userAtom = atom({ username: '', token: '' });
